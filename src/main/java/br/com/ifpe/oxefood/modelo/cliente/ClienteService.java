efbc3afb5984f1dd6bc8c1c321f5c7b5bb8591ff
deyvsonflima <dfsl2@discente.ifpe.edu.br>
2023-05-16 19:54:35 -0300

    curso de web 3 - api

---
+++ src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
@@ -0,0 +1,36 @@
+package br.com.ifpe.oxefood.modelo.cliente;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.ifpe.oxefood.util.entity.GenericService;
+
+@Service
+public class ClienteService extends GenericService {
+    
+    @Autowired
+    private ClienteRepository repository;
+
+    @Transactional
+    public Cliente save(Cliente cliente) {
+
+        super.preencherCamposAuditoria(cliente);
+        return repository.save(cliente);
+    }
+
+    public List<Cliente> listarTodos() {
+  
+        return repository.findAll();
+    }
+ 
+    public Cliente obterPorID(Long id) {
+ 
+        return repository.findById(id).get();
+    }
+ 
+
+}
