1118d5960476c55793de5572af9f0b3ab9d6d955
deyvsonflima <dfsl2@discente.ifpe.edu.br>
2023-05-16 21:13:04 -0300

    curso de web 3 1 unidade

--- src/test/java/br/com/ifpe/oxefood/OxefoodApiDeyvsonApplicationTests.java
+++ src/test/java/br/com/ifpe/oxefood/OxefoodApiDeyvsonApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OxefoodApiTardeApplicationTests {
+class OxefoodApiDeyvsonApplicationTests {
 
 	@Test
 	void contextLoads() {
