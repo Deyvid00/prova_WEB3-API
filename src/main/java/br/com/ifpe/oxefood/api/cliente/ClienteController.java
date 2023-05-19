4ea70429ac0848f551eb7a637fbef75807009e2d
deyvsonflima <dfsl2@discente.ifpe.edu.br>
2023-05-19 17:31:50 -0300

    Implementando a Alteração

--- src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteController.java
+++ src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,7 @@ import br.com.ifpe.oxefood.util.entity.GenericController;
 @RestController
 @RequestMapping("/api/cliente")
 public class ClienteController extends GenericController {
-    
+
     @Autowired
     private ClienteService clienteService;
 
@@ -35,14 +36,21 @@ public class ClienteController extends GenericController {
 
     @GetMapping
     public List<Cliente> listarTodos() {
-  
-       return clienteService.listarTodos();
+
+        return clienteService.listarTodos();
     }
 
     @GetMapping("/{id}")
     public Cliente obterPorID(@PathVariable Long id) {
 
-       return clienteService.obterPorID(id);
+        return clienteService.obterPorID(id);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> update(@PathVariable("id") Long id, @RequestBody ClienteRequest request) {
+
+        clienteService.update(id, request.build());
+        return ResponseEntity.ok().build();
     }
 
 }
--- src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
@@ -11,7 +11,7 @@ import br.com.ifpe.oxefood.util.entity.GenericService;
 
 @Service
 public class ClienteService extends GenericService {
-    
+
     @Autowired
     private ClienteRepository repository;
 
@@ -23,14 +23,27 @@ public class ClienteService extends GenericService {
     }
 
     public List<Cliente> listarTodos() {
-  
+
         return repository.findAll();
     }
- 
+
     public Cliente obterPorID(Long id) {
- 
+
         return repository.findById(id).get();
     }
- 
+
+    @Transactional
+    public void update(Long id, Cliente clienteAlterado) {
+
+        Cliente cliente = repository.findById(id).get();
+        cliente.setNome(clienteAlterado.getNome());
+        cliente.setDataNascimento(clienteAlterado.getDataNascimento());
+        cliente.setCpf(clienteAlterado.getCpf());
+        cliente.setFoneCelular(clienteAlterado.getFoneCelular());
+        cliente.setFoneFixo(clienteAlterado.getFoneFixo());
+
+        super.preencherCamposAuditoria(cliente);
+        repository.save(cliente);
+    }
 
 }
